f9b18de3e893d25abf6a7cb4c027237cb9f3cb1f
Martin Trubíni <martin.trubini@hypertext.sk>
2024-09-02 19:38:09 +0200

    WSL Xdebuig fix - script improvements

--- wsl-xdebug-fix/wsl-xdebug-fix.sh
+++ wsl-xdebug-fix/wsl-xdebug-fix.sh
@@ -1,3 +1,24 @@
+#!/bin/bash
+
+# Ask the user for the PHP version
+read -p "Enter the PHP version you want to fix (e.g., 7.4, 8.0): " php_version
+
+# Verify PHP is installed
+if command -v php"$php_version" >/dev/null 2>&1; then
+    echo "✓ PHP version $php_version is installed."
+else
+    echo "PHP version $php_version is not installed."
+    echo "Exiting"
+    exit 1
+fi
+
+# Obtain Windows IP address
 IP=$(ip route show | grep -i default | awk '{ print $3 }')
 
-sed -i "s/xdebug.client_host=.*/xdebug.client_host=$IP/g" /etc/php/8.2/apache2/php.ini
+# Set Windows IP address to Xdebug configurtaion
+sed -i "s/xdebug.client_host=.*/xdebug.client_host=$IP/g" /etc/php/$php_version/apache2/php.ini
+
+# Restart Apache service
+echo "Restarting Apache service ..."
+systemctl restart apache2
+
